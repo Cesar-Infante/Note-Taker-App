1de8d8ccf3a642a9b02ca41b358f40c289e7284e
cesar-infante <cesar261110@gmail.com>
2022-08-02 16:16:20 -0700

    server.js no working

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 /* Importing the express module. */
-const express = require('express')
+const express = require('express');
 
 /* This is a way to set the port number. The first part is for when the app is deployed to Heroku. The
 second part is for when the app is run locally. */
@@ -8,12 +8,26 @@ const PORT = process.env.PORT || 3001;
 /* Creating an instance of the express module. */
 const app = express();
 
+const path = require('path');
+
+const fs = require('fs');
+
+const data = require('./db/db.json');
+
+app.use(express.json())
+
 /* This is telling the app to use the public folder as a static directory. */
 app.use(express.static('public'));
 
 app.get('/api/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+app.get('/api/db', (req, res) => res.json(data))
 
 app.listen(PORT, () => 
     console.log(`Example app listening at http://localhost:${PORT}`))
