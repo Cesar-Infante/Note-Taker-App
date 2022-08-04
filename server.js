b021c1d5ccd92b9763dfe39f70f1c2a0e986467f
cesar-infante <cesar261110@gmail.com>
2022-08-04 11:07:15 -0700

    fix to /notes

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.get('/', (req, res) => {
 });
 
 /* This is telling the app to send the notes.html file when the user goes to the note file route*/
-app.get('/note', (req, res) => {
+app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
 
